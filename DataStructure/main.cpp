a6d6462e24e78dd15690d9c7b21b475348c2b2e6
이광용 <rhkdrmfh@gmail.com>
2019-10-14 09:40:58 +0900

    Initial Commit

---
+++ DataStructure/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DataStructure
+//
+//  Created by 이광용 on 2019/10/14.
+//  Copyright © 2019 GwangYongLee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
