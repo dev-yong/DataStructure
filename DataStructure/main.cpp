a042a2b06cab53d5af7b5258c113dc9199bc5eb9
이광용 <rhkdrmfh@gmail.com>
2019-10-17 13:17:52 +0900

    Update main.cpp
    Tests for AbstractList(LinkedList), AbstractSortedList(BST)

--- DataStructure/main.cpp
+++ DataStructure/main.cpp
@@ -7,12 +7,11 @@
 //
 
 #include <iostream>
-#include "AbstractList.hpp"
-#include "List/AbstractList_LinkedList.cpp"
+#include "AbstractDataType.hpp"
 
 using namespace std;
 
-int main() {
+void testAbstractList_LinkedList() {
     AbstractList<int>* list = new AbstractList_LinkedList<int>();
     list->insertAfter(0, 1);
     list->insertAfter(0, 2);
@@ -24,6 +23,26 @@ int main() {
     list->insertAfter(0, 8);
     list->log();
     delete list;
+}
 
+void testAbstractSortedList_BST() {
+    AbstractSortedList<int>* sortedList = new AbstractSortedList_BST<int>();
+    sortedList->insert(5);
+    sortedList->insert(3);
+    sortedList->insert(4);
+    sortedList->insert(2);
+    sortedList->insert(10);
+    sortedList->insert(8);
+                       
+    delete sortedList;
+}
+
+
+int main() {
+
+    testAbstractList_LinkedList();
+
+    testAbstractSortedList_BST();
+    
     return 0;
 }
